c8f36b3a26ad2e9b9042df2a9a81e6cd510fac9d
Harold <harmed2002@hotmail.com>
2023-04-22 01:43:57 -0500

    Se hacen los cambios sugeridos por el tutor. Se modifica el ciclo para que pida varios creditos, Se crean los objetos del programa.

--- functions.js
+++ functions.js
@@ -1,31 +1,74 @@
-let lnVlrPrestamo = 0;
-let lnNumCuotas = 0;
-const lnTasa = 0.02;
-let lnVlrCuota = 0;
-let lnSw = false;
+let nombre, vlrPrestamo = 0, numCuotas = 0, vlrCuota = 0, entrarCiclo = true, listaPrestamos = []
+const tasaInt = 0.02;
 
-// Solicito los valores
-while (!lnSw) {
-    lnVlrPrestamo = parseInt(prompt("Digita el valor del préstamo: "));
-    lnNumCuotas = parseInt(prompt("Digita el número de cuotas: "));
-
-    if (!isNaN(lnVlrPrestamo) && !isNaN(lnNumCuotas)) {
-        lnSw = true;
-    } else {
-        alert("Alguno de los valores no es un número. intente nuevamente.")
-    }
+// Creo la clase préstamo
+const Prestamo = function (nombre, vlrPrestamo, numCuotas, tasaInt, vlrCuota) {
+	this.nombre = nombre,
+	this.vlrPrestamo = vlrPrestamo,
+	this.numCuotas = numCuotas,
+	this.tasaInt = tasaInt,
+	this.vlrCuota = vlrCuota
 }
 
-// llamo la función de cálculo de cuota
-lnVlrCuota = obtenerCuotaMensual(lnVlrPrestamo, lnNumCuotas, lnTasa);
-alert(`Sr. usuario, el valor de la cuota mensual es: ${lnVlrCuota} a una tasa del ${lnTasa * 100}%`);
+// Solicito los valores
+while (entrarCiclo) {
+	nombre = prompt("Digita el nombre del cliente: ").toLowerCase();
+	
+	// Verifico que el nombre no esté vacío
+	if (!nombre) {
+		alert("Sr. usuario, el nombre no puede estar vacío. Intente nuevamente.");
+		continue;
 
+	} else {
+		
+		// Verifico que el nombre no exista ya con un préstamo (un cliente solo puede tener un préstamo activo)
+		if (tienePrestamo(nombre)) {
+			alert("Sr. usuario, este cliente ya tiene un préstamo activo. Intente nuevamente.");
+			continue;
+		}
+	}
 
-function obtenerCuotaMensual(pVlrPrestamo, pNumCuotas, pTasa) {
-    let lnVlrInteres = pVlrPrestamo * pTasa * pNumCuotas;
-    let lnVlrCtaMensual = (pVlrPrestamo + lnVlrInteres) / pNumCuotas;
-    
-    return parseFloat(lnVlrCtaMensual.toFixed(2));
+	vlrPrestamo = parseInt(prompt("Digita el valor del préstamo: "));
+	numCuotas = parseInt(prompt("Digita el número de cuotas: "));
 
-    //return lnVlrCtaMensual;
+	if (!isNaN(vlrPrestamo) && !isNaN(numCuotas)) {
+		if (vlrPrestamo > 0 && numCuotas > 0) {
+
+			ingresarPrestamo(nombre, vlrPrestamo, numCuotas);
+
+			entrarCiclo = confirm("¿Desea ingresar un nuevo crédito?");
+
+		} else {
+			alert("Sr. usuario, los valores deben ser mayores que 0. Intente nuevamente.");
+		}
+
+	} else {
+		alert("Sr. usuario, alguno(s) de los valores no es un número. Intente nuevamente.");
+	}
+}
+
+function ingresarPrestamo(nombre, vlrPrestamo, numCuotas) {
+	// llamo la función de cálculo de cuota
+	vlrCuota = obtenerCuotaMensual(vlrPrestamo, numCuotas, tasaInt);
+	alert(`Sr. usuario, el valor de la cuota mensual es: ${vlrCuota} a una tasa del ${tasaInt * 100}%`);
+
+	// Instancio Prestamo
+	let prestamo = new Prestamo(nombre, vlrPrestamo, numCuotas, tasaInt, vlrCuota);
+
+	// Ingreso el prestamo en la lista
+	listaPrestamos.push(prestamo);
+	console.log(listaPrestamos);
+
+}
+
+function obtenerCuotaMensual(valorPrestamo, numeroCuotas, tasaInteres) {
+	let valorInteres = valorPrestamo * tasaInteres * numeroCuotas;
+	let valorCuotaMensual = (valorPrestamo + valorInteres) / numeroCuotas;
+	valorCuotaMensual = Math.round(valorCuotaMensual);
+
+	return valorCuotaMensual;
+}
+
+function tienePrestamo(nombre) {
+	return listaPrestamos.some((el) => el.nombre == nombre);
 }
